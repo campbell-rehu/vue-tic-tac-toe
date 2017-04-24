c8cc0e13647cb166eff9c8c0f2091b377d7fcb0e
campbell <campbellrehu@me.com>
2017-04-24 17:46:42 +1200

    Updated Cell Vue file

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,5 +1,5 @@
 <template>
-    <td class="cell">{{mark}}</td>
+    <td class="cell" @click="strike">{{mark}}</td>
 </template>
 
 <script>
@@ -8,6 +8,15 @@
             return {
                 frozen: false,
                 mark: ''
+            }
+        },
+        methods: {
+            strike() {
+                if (!this.frozen) {
+                    this.mark = this.$parent.activePlayer;
+                    this.frozen = true;
+                    Event.$emit('strike', this.name);
+                }
             }
         }
     }
